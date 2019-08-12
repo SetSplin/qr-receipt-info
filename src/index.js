da092a27db935af3140d3d4fe95a5503224925ad
Igor Tarakanov <igortarakanov@mail.ru>
2019-08-13 00:33:12 +0300

    initial

---
+++ src/index.js
@@ -0,0 +1,78 @@
+import parseCheckString from './parseCheckString';
+import fetch from 'node-fetch';
+import { Base64 } from 'js-base64'
+
+
+const URL_CHECK = "https://proverkacheka.nalog.ru:9999/v1/ofds/*/inns/*/fss/<fiscalNumber>/operations/<checkType>/tickets/<fiscalDocument>?fiscalSign=<fiscalSign>&date=<time>&sum=<price>";
+const URL_GET = "https://proverkacheka.nalog.ru:9999/v1/inns/*/kkts/*/fss/<fiscalNumber>/tickets/<fiscalDocument>?fiscalSign=<fiscalSign>&sendToEmail=no";
+
+
+export default class CheckInfoGetter {
+  constructor({ username, password, delay }) {
+    this.username = username;
+    this.password = password;
+    this.delay = delay;
+  }
+
+  static makeUrl(url_template, params) {
+    let url = url_template.slice();
+    Object.keys(params).forEach(param => {
+      url = url.replace(`<${param}>`, params[param]);
+    });
+    return url;
+  }
+
+  getHeaders() {
+    return {
+      'Authorization':
+        'Basic ' + Base64.encode(this.username + ":" + this.password),
+      'Device-Id': '',
+      'Device-OS' : '',
+    };
+  }
+
+  async request(url) {
+    return fetch(
+      url,
+      {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: this.getHeaders(),
+        redirect: 'follow',
+        referrer: 'no-referrer',
+      }
+    );
+  }
+
+  async get(qr_string) {
+    try {
+      const params = parseCheckString(qr_string);
+      if (params === null) {
+        return null;
+      }
+
+      const check_status = (await this.request(
+        CheckInfoGetter.makeUrl(URL_CHECK, params)
+      )).status;
+
+      if (check_status === 204) {
+        // delay required between check and get
+        return new Promise((resolve) => {
+          setTimeout(async () => {
+            try {
+              const result = (await this.request(
+                CheckInfoGetter.makeUrl(URL_GET, params)
+              ));
+              resolve((await result.json()).document.receipt);
+            } catch {
+              resolve(null);
+            }
+          }, this.delay);
+        });
+      }
+    } catch {}
+    return null;
+  }
+}
