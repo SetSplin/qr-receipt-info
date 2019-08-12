db1402af0dd468ca2c403d0a3e1649c272b6bdd0
Igor Tarakanov <igortarakanov@mail.ru>
2019-08-13 00:44:27 +0300

    fix babel

--- dist/index.js
+++ dist/index.js
@@ -13,188 +13,76 @@ var _jsBase = require("js-base64");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+const URL_CHECK = "https://proverkacheka.nalog.ru:9999/v1/ofds/*/inns/*/fss/<fiscalNumber>/operations/<checkType>/tickets/<fiscalDocument>?fiscalSign=<fiscalSign>&date=<time>&sum=<price>";
+const URL_GET = "https://proverkacheka.nalog.ru:9999/v1/inns/*/kkts/*/fss/<fiscalNumber>/tickets/<fiscalDocument>?fiscalSign=<fiscalSign>&sendToEmail=no";
 
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var URL_CHECK = "https://proverkacheka.nalog.ru:9999/v1/ofds/*/inns/*/fss/<fiscalNumber>/operations/<checkType>/tickets/<fiscalDocument>?fiscalSign=<fiscalSign>&date=<time>&sum=<price>";
-var URL_GET = "https://proverkacheka.nalog.ru:9999/v1/inns/*/kkts/*/fss/<fiscalNumber>/tickets/<fiscalDocument>?fiscalSign=<fiscalSign>&sendToEmail=no";
-
-var CheckInfoGetter =
-/*#__PURE__*/
-function () {
-  function CheckInfoGetter(_ref) {
-    var username = _ref.username,
-        password = _ref.password;
-
-    _classCallCheck(this, CheckInfoGetter);
-
+class CheckInfoGetter {
+  constructor({
+    username,
+    password,
+    delay
+  }) {
     this.username = username;
     this.password = password;
+    this.delay = delay;
   }
 
-  _createClass(CheckInfoGetter, [{
-    key: "getHeaders",
-    value: function getHeaders() {
-      return {
-        'Authorization': 'Basic ' + _jsBase.Base64.encode(this.username + ":" + this.password),
-        'Device-Id': '',
-        'Device-OS': ''
-      };
-    }
-  }, {
-    key: "request",
-    value: function () {
-      var _request = _asyncToGenerator(
-      /*#__PURE__*/
-      regeneratorRuntime.mark(function _callee(url) {
-        return regeneratorRuntime.wrap(function _callee$(_context) {
-          while (1) {
-            switch (_context.prev = _context.next) {
-              case 0:
-                return _context.abrupt("return", (0, _nodeFetch.default)(url, {
-                  method: 'GET',
-                  mode: 'cors',
-                  cache: 'no-cache',
-                  credentials: 'same-origin',
-                  headers: this.getHeaders(),
-                  redirect: 'follow',
-                  referrer: 'no-referrer'
-                }));
+  static makeUrl(url_template, params) {
+    let url = url_template.slice();
+    Object.keys(params).forEach(param => {
+      url = url.replace(`<${param}>`, params[param]);
+    });
+    return url;
+  }
 
-              case 1:
-              case "end":
-                return _context.stop();
-            }
-          }
-        }, _callee, this);
-      }));
+  getHeaders() {
+    return {
+      'Authorization': 'Basic ' + _jsBase.Base64.encode(this.username + ":" + this.password),
+      'Device-Id': '',
+      'Device-OS': ''
+    };
+  }
 
-      function request(_x) {
-        return _request.apply(this, arguments);
+  async request(url) {
+    return (0, _nodeFetch.default)(url, {
+      method: 'GET',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: this.getHeaders(),
+      redirect: 'follow',
+      referrer: 'no-referrer'
+    });
+  }
+
+  async get(qr_string) {
+    try {
+      const params = (0, _parseCheckString.default)(qr_string);
+
+      if (params === null) {
+        return null;
       }
 
-      return request;
-    }()
-  }, {
-    key: "get",
-    value: function () {
-      var _get = _asyncToGenerator(
-      /*#__PURE__*/
-      regeneratorRuntime.mark(function _callee3(qr_string) {
-        var _this = this;
+      const check_status = (await this.request(CheckInfoGetter.makeUrl(URL_CHECK, params))).status;
 
-        var params, check_status;
-        return regeneratorRuntime.wrap(function _callee3$(_context3) {
-          while (1) {
-            switch (_context3.prev = _context3.next) {
-              case 0:
-                _context3.prev = 0;
-                params = (0, _parseCheckString.default)(qr_string);
-
-                if (!(params === null)) {
-                  _context3.next = 4;
-                  break;
-                }
-
-                return _context3.abrupt("return", null);
-
-              case 4:
-                _context3.next = 6;
-                return this.request(CheckInfoGetter.makeUrl(URL_CHECK, params));
-
-              case 6:
-                check_status = _context3.sent.status;
-
-                if (!(check_status === 204)) {
-                  _context3.next = 9;
-                  break;
-                }
-
-                return _context3.abrupt("return", new Promise(function (resolve) {
-                  setTimeout(
-                  /*#__PURE__*/
-                  _asyncToGenerator(
-                  /*#__PURE__*/
-                  regeneratorRuntime.mark(function _callee2() {
-                    var result;
-                    return regeneratorRuntime.wrap(function _callee2$(_context2) {
-                      while (1) {
-                        switch (_context2.prev = _context2.next) {
-                          case 0:
-                            _context2.prev = 0;
-                            _context2.next = 3;
-                            return _this.request(CheckInfoGetter.makeUrl(URL_GET, params));
-
-                          case 3:
-                            result = _context2.sent;
-                            _context2.t0 = resolve;
-                            _context2.next = 7;
-                            return result.json();
-
-                          case 7:
-                            _context2.t1 = _context2.sent.document.receipt;
-                            (0, _context2.t0)(_context2.t1);
-                            _context2.next = 14;
-                            break;
-
-                          case 11:
-                            _context2.prev = 11;
-                            _context2.t2 = _context2["catch"](0);
-                            resolve(null);
-
-                          case 14:
-                          case "end":
-                            return _context2.stop();
-                        }
-                      }
-                    }, _callee2, null, [[0, 11]]);
-                  })), 500);
-                }));
-
-              case 9:
-                _context3.next = 13;
-                break;
-
-              case 11:
-                _context3.prev = 11;
-                _context3.t0 = _context3["catch"](0);
-
-              case 13:
-                return _context3.abrupt("return", null);
-
-              case 14:
-              case "end":
-                return _context3.stop();
+      if (check_status === 204) {
+        // delay required between check and get
+        return new Promise(resolve => {
+          setTimeout(async () => {
+            try {
+              const result = await this.request(CheckInfoGetter.makeUrl(URL_GET, params));
+              resolve((await result.json()).document.receipt);
+            } catch {
+              resolve(null);
             }
-          }
-        }, _callee3, this, [[0, 11]]);
-      }));
-
-      function get(_x2) {
-        return _get.apply(this, arguments);
+          }, this.delay);
+        });
       }
+    } catch {}
 
-      return get;
-    }()
-  }], [{
-    key: "makeUrl",
-    value: function makeUrl(url_template, params) {
-      var url = url_template.slice();
-      Object.keys(params).forEach(function (param) {
-        url = url.replace("<".concat(param, ">"), params[param]);
-      });
-      return url;
-    }
-  }]);
+    return null;
+  }
 
-  return CheckInfoGetter;
-}();
+}
 
 exports.default = CheckInfoGetter;
--- dist/parseCheckString.js
+++ dist/parseCheckString.js
@@ -4,22 +4,11 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = parseCheckString;
-
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
-
-function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-
-var formats = [{
+const formats = [{
   t: 'time',
   s: {
     name: 'price',
-    transformer: function transformer(price) {
-      return parseInt(price.replace('.', ''));
-    }
+    transformer: price => parseInt(price.replace('.', ''))
   },
   fn: 'fiscalNumber',
   i: 'fiscalDocument',
@@ -28,8 +17,8 @@ var formats = [{
 }];
 
 function match(params, format) {
-  var formatKeys = Object.keys(format);
-  Object.keys(params).forEach(function (param) {
+  const formatKeys = Object.keys(format);
+  Object.keys(params).forEach(param => {
     if (!(param in formatKeys)) {
       return false;
     }
@@ -38,30 +27,22 @@ function match(params, format) {
 }
 
 function extract(params, format) {
-  var result = {};
-  Object.keys(params).forEach(function (param) {
-    var normal_name = typeof format[param] === 'string' ? format[param] : format[param].name;
-    var transformer = typeof format[param] === 'string' ? function (el) {
-      return el;
-    } : format[param].transformer;
+  const result = {};
+  Object.keys(params).forEach(param => {
+    const normal_name = typeof format[param] === 'string' ? format[param] : format[param].name;
+    const transformer = typeof format[param] === 'string' ? el => el : format[param].transformer;
     result[normal_name] = transformer(params[param]);
   });
   return result;
 }
 
 function parseCheckString(qr_string) {
-  var params = {};
-  qr_string.split('&').forEach(function (param) {
-    var _param$split = param.split('='),
-        _param$split2 = _slicedToArray(_param$split, 2),
-        name = _param$split2[0],
-        value = _param$split2[1];
-
+  const params = {};
+  qr_string.split('&').forEach(param => {
+    const [name, value] = param.split('=');
     params[name] = value;
   });
-  var format = formats.find(function (_format) {
-    return match(params, _format);
-  });
+  const format = formats.find(_format => match(params, _format));
 
   if (format === null) {
     return null;
